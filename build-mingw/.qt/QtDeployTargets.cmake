3a1c1ecfd4187509e5d9a67dc0ad1fd88c9355f2
C0ngRB <congruibo@163.com>
2026-01-30 17:36:06 +0800

    删除build-mingw文件夹，添加到.gitignore

--- build-mingw/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GasDispersionDesktop_FILE C:/Users/congr/Desktop/GasDispersionDesktop/build-mingw/GasDispersionDesktop.exe)
-set(__QT_DEPLOY_TARGET_GasDispersionDesktop_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GasDispersionDesktop_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
--- build-mingw/CMakeFiles/GasDispersionDesktop.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GasDispersionDesktop.
-# This may be replaced when dependencies are built.
